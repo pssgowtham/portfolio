c3eedd3775409e36fa09c9e6625d15268d1e759e
pssgowtham <gowthamsps98@gmail.com>
2024-11-18 11:29:48 -0800

    Under Construction

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/portfolio', // Replace <repository-name> with your GitHub repository name
+  base: '/portfolio/', // Replace <repository-name> with your GitHub repository name
   plugins: [react()],
 });
